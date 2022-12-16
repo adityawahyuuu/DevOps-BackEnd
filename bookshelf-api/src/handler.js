19773569070974b2826284c3462502f3f5c38753
adityawahyuuu <pradhanaaditya30@gmail.com>
2022-12-16 22:28:02 +0700

    bookshelf-api handler v3

--- bookshelf-api/src/handler.js
+++ bookshelf-api/src/handler.js
@@ -1,25 +1,15 @@
 const {existingBook, findAllData, Book} = require('./checkBook');
 
-// const handlerFunc = (h, status = undefined, message = undefined, httpCode = undefined, data = undefined) => {
-    //     const response = h.response({
-//         status: status,
-//         message: message,
-//         data: data,
-//     });
-//     response.code(httpCode);
-//     return response;
-// };
-
 const onlySpaces = (str) => {
     return str.trim().length === 0;
 };
 
-// Route Handler
+// ----------------------------------------- //
+// ------------- Route Handler ------------- //
+// ----------------------------------------- //
 const getAllBooksHandler = async (req, h) => {
     try{
         const bookArray = await findAllData();
-        // if(bookArray.length > 0){
-        // }
         return h.view('all-book', {
             bookArray
         });
@@ -39,20 +29,17 @@ const formAddBookHandler = async (req, h) => {
 
 const addBookHandler = async (req, h) => {
     try{
-        // // Mengambil nilai dari masing-masing property
         const data = req.payload;
         const {name, year, author, summary, publisher, pageCount, readPage} = data;
         const finished = (pageCount === readPage) ? true:false;
         const insertedAt = new Date().toString();
         const updatedAt = insertedAt;
 
-        // Menggabungkan ke dalam satu json object newBook
         const newBook = {
             name, year, author, summary, publisher, pageCount, readPage, finished, insertedAt, updatedAt
         };
         const book = new Book(newBook);
 
-        // duplikat && string name whitespace? save:don't
         const filterBook = {
             name: name,
             year: year,
@@ -62,8 +49,6 @@ const addBookHandler = async (req, h) => {
         if(id === null){
             if(onlySpaces(name)){
                 return h.view('empty', {message: `Nama Buku tidak Boleh Hanya Whitespace`});
-                // const whiteSpaceBookName = handlerFunc(h, 'fail', 'Nama Buku tidak Boleh Hanya Whitespace', 400);
-                // return whiteSpaceBookName;
             } else if(parseInt(readPage) > parseInt(pageCount)){
                 return h.view('empty', {message: `Read Page tidak Boleh Lebih dari Page Count`});
             }
@@ -84,13 +69,6 @@ const getBookByIdHandler = async (req, h) => {
         const bookArray = await findAllData();
         const books = bookArray.filter(book => book._id == id);
         if(books.length > 0){
-            // const bookInfo = handlerFunc(h, 'success', undefined, 200, {
-            //     id: books[0].id,
-            //     name: books[0].name,
-            //     year: books[0].year,
-            //     author: books[0].author
-            // });
-            // return bookInfo;
             return h.view('by-id-view', {
                 id: books[0].id,
                 name: books[0].name,
@@ -105,8 +83,6 @@ const getBookByIdHandler = async (req, h) => {
             });
         }
         return h.view('empty', {message: `Buku tidak ditemukan`})
-        // const bookInfo = handlerFunc(h, 'fail', 'Buku tidak ditemukan', 404);
-        // return bookInfo;
     } catch(e){
         return h.view('empty', {message: `${e}`});
     }
@@ -134,12 +110,8 @@ const editBookByIdHandler = async (req, h) => {
         
         if(index != -1){
             if(onlySpaces(name)){
-                // const whiteSpaceBookName = handlerFunc(h, 'fail', 'Nama Buku tidak Boleh Hanya Whitespace', 400);
-                // return whiteSpaceBookName;
                 return h.view('empty', {message: `Nama Buku tidak Boleh Hanya Whitespace`});
             } else if(parseInt(readPage) > parseInt(pageCount)){
-                // const pageNumber = handlerFunc(h, 'fail', 'readPage tidak boleh lebih besar dari pageCount', 400);
-                // return pageNumber;
                 return h.view('empty', {message: `readPage tidak boleh lebih besar dari pageCount`});
             }
             bookArray[index].name = name;
@@ -158,8 +130,6 @@ const editBookByIdHandler = async (req, h) => {
             });
         } else{
             return h.view('empty', {message: `Gagal memperbarui buku. Id tidak ditemukan`});
-            // const errorId = handlerFunc(h, 'fail', 'Gagal memperbarui buku. Id tidak ditemukan', 404);
-            // return errorId;
         }
         
     } catch(e){
@@ -183,7 +153,6 @@ const deleteBookByIdHandler = async (req, h) => {
         const bookArray = await findAllData();
         const index = bookArray.findIndex(book => (book._id == id) && (id == data.id));
         if(index != -1){
-            // Delete mongodb document id
             Book.deleteOne({_id: bookArray[index].id}, (err, doc) => {
                 if(err){
                     console.log(err);
@@ -197,8 +166,6 @@ const deleteBookByIdHandler = async (req, h) => {
             });
         } else{
             return h.view('empty', {message: `Buku gagal dihapus. Id yang anda masukan tidak sesuai`});
-            // const errorDelete = handlerFunc(h, 'fail', 'Buku gagal dihapus. Id yang anda masukan tidak sesuai', 404);
-            // return errorDelete;
         }
     } catch(e){
         return h.view('empty', {message: `${e}`});
