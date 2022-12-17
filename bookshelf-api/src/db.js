b43d746b836850f44f197de571cc5adf04add3a0
adityawahyuuu_ <75268457+adityawahyuuu@users.noreply.github.com>
2022-12-17 20:41:55 +0700

    Update db.js

--- bookshelf-api/src/db.js
+++ bookshelf-api/src/db.js
@@ -3,4 +3,4 @@ const mongoose = require('mongoose');
 const options = {
     dbName: "noob-data",
 };
-mongoose.connect('mongodb+srv://aditya:human567@cluster0.hxrc6b2.mongodb.net/?retryWrites=true&w=majority', options);
+mongoose.connect('mongodb+srv://username:password@cluster0.hxrc6b2.mongodb.net/?retryWrites=true&w=majority', options);
