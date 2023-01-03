2d91e386ce4cecfabfd736f9736a49a3daa52121
root <root@LAPTOP-NTF08U7S.localdomain>
2023-01-03 15:08:36 +0700

    Setup Route

--- temp-realtime-monitoring/src/app.js
+++ temp-realtime-monitoring/src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('better-config');
+const bodyParser = require('body-parser');
+const router = require('./routes');
 
 config.set('../config.json');
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use('/', router);
 
 const port = config.get('application.port');
 const host = config.get('application.host');
